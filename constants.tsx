1f941d4fa5aa092aeb8b2e2a9ec8e947a1eb61cc
joaojoaojoao11 <joao.vanderleia@gmail.com>
2026-01-18 16:10:52 -0300

    feat: Add filter functionality to Accounts Payable
    Introduces advanced filtering options for the Accounts Payable view, including date range, status, and category. A new Filter icon has been added to the constants.

--- constants.tsx
+++ constants.tsx
@@ -58,5 +58,8 @@ export const ICONS = {
   ),
   Notification: (props: any) => (
     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" {...props}><path d="M18 8A6 6 0 0 0 6 8c0 7-3 9-3 9h18s-3-2-3-9"/><path d="M13.73 21a2 2 0 0 1-3.46 0"/></svg>
+  ),
+  Filter: (props: any) => (
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" {...props}><polygon points="22 3 2 3 10 12.46 10 19 14 21 14 12.46 22 3"/></svg>
   )
 };
